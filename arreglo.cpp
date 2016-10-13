88a9840f2949d0d439bbb0f0e30380835a88b186
AlbertoGarcia <alberto.lomely@hotmail.com>
2016-10-13 17:15:26 -0500

    rama prueba

--- arreglo.cpp
+++ arreglo.cpp
@@ -1,20 +1,20 @@
 #include<stdio.h>
 #include<conio.h>
 int main(){
-int x[25];
+int x[5];
 int i,j,a;
-for (i=0; i <25; i=i++){
+for (i=0; i <5; i=i++){
 printf("De valor de x[%d]:", i+1);
 scanf("%d",&x[i]);
 }
-for (i=0; i <25; i=i++)
-for (j=0; j <25; j=j++)
+for (i=0; i <5; i=i++)
+for (j=0; j <5; j=j++)
 if ( x[i] < x[j] ){
 a= x[i];
 x[i]=x[j];
 x[j]=a;
 }
-for(i=0; i <25; i=i++)
+for(i=0; i <5; i=i++)
 printf("%3d", x[i]);
 getch();
 return 0;
