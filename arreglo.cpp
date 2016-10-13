f8161319b076147dd82148511908e00aefe1e433
AlbertoGarcia <alberto.lomely@hotmail.com>
2016-10-13 16:42:55 -0500

    arreglo de 15

--- arreglo.cpp
+++ arreglo.cpp
@@ -1,20 +1,20 @@
 #include<stdio.h>
 #include<conio.h>
 int main(){
-int x[10];
+int x[15];
 int i,j,a;
-for (i=0; i <10; i=i++){
+for (i=0; i <15; i=i++){
 printf("De valor de x[%d]:", i+1);
 scanf("%d",&x[i]);
 }
-for (i=0; i <10; i=i++)
-for (j=0; j <10; j=j++)
+for (i=0; i <15; i=i++)
+for (j=0; j <15; j=j++)
 if ( x[i] < x[j] ){
 a= x[i];
 x[i]=x[j];
 x[j]=a;
 }
-for(i=0; i <10; i=i++)
+for(i=0; i <15; i=i++)
 printf("%3d", x[i]);
 getch();
 return 0;
